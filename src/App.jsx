3b91b541153c32e1516b21ca05d1511a9a02db50
AlfrinP <alfrinpoulose@gmail.com>
2023-11-21 19:14:52 +0530

    added facutly page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import Student from "./components/Dashboard/Student"
-import Login from "./components/Login/Login"
+import Faculty from "./components/Dashboard/Faculty";
+import Student from "./components/Dashboard/Student";
+import Login from "./components/Login/Login";
 
 function App() {
-
   return (
     <>
-      <Login/>
+      <Login />
       <Student/>
+      <Faculty />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Dashboard/Student.jsx
+++ src/components/Dashboard/Student.jsx
@@ -9,19 +9,19 @@ export default function Student() {
   return (
     <div className="w-full h-screen">
       <Navbar />
-      <div className="w-full height center flex-col gap-5">
-        <div className="w-full center justify-evenly">
-          <div className="ring-offset-8 ring-2 ring-[#512B81] rounded-full w-32">
+      <div className="w-full height center grid grid-cols-6">
+        <div className="col-span-1"></div>
+          <div className="ring-offset-8 ring-2 ring-[#512B81] rounded-full w-32 col-span-1">
             <img src={sprofile} className="rounded-full" />
           </div>
-          <div className="center flex-col gap-3 items-start">
+          <div className="center flex-col gap-3 items-start col-span-2">
             <div className="center flex-col items-start">
               <span className="font-normal text-[#512B81]">Student Name</span>
               <span className="text-lg font-semibold">
                 Francis Gigi Kuyilamparambil
               </span>
             </div>
-            <div className="center w-full gap-3">
+            <div className="center justify-start w-full gap-3">
               <div className="center flex-col items-start bg-[#512B81] rounded-xl w-[180px] py-1 px-2 justify-evenly text-white">
                 <span className="font-light">Register No:</span>
                 <span className="font-semibold">CCE23CS032</span>
@@ -36,7 +36,7 @@ export default function Student() {
               <span className="text-2xl text-[#512B81]">4</span>
             </div>
           </div>
-          <div className="center flex-col gap-3">
+          <div className="center flex-col gap-3 col-span-1">
             <button className="center bg-[#512B81] rounded-xl w-full p-3 justify-evenly">
               <img src={upload} width={30}/>
               <span className=" text-white">Upload Certificate</span>
@@ -46,10 +46,12 @@ export default function Student() {
               <span className=" text-white">View Points</span>
             </button>
           </div>
-        </div>
-        <div className="w-[1000px] center shadow-[0_3px_10px_rgb(0,0,0,0.2)] text-black rounded-lg">
+          <div className="col-span-1"></div>
+          <div className="col-span-1"></div>
+        <div className="center shadow-[0_3px_10px_rgb(0,0,0,0.2)] text-black rounded-lg col-span-4 row-span-1">
           <Table />
         </div>
+        <div className="col-span-1"></div>
       </div>
     </div>
   );
