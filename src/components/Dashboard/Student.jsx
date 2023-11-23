93c55f8ff86b0f1b236a84593c6adfa44fc9efbd
AlfrinP <alfrinpoulose@gmail.com>
2023-11-23 11:06:08 +0530

    resolved conflict

--- src/components/Dashboard/Student.jsx
+++ src/components/Dashboard/Student.jsx
@@ -11,23 +11,22 @@ export default function Student() {
   const [openFileModal, setOpenFileModal] = React.useState(false);
   const [openViewPoints, setOpenViewPoints] = React.useState(false);
 
- 
   const handleOpenFileModal = () => setOpenFileModal(!openFileModal);
   const handleOpenViewPoints = () => setOpenViewPoints(!openViewPoints);
   return (
     <div className="w-full h-screen">
       <Navbar />
-<<<<<<< HEAD
-      <div className="w-full height center grid grid-cols-6">
-        <div className="col-span-1"></div>
-          <div className="ring-offset-8 ring-2 ring-[#512B81] rounded-full w-32 col-span-1">
-=======
-      <FileUploadModel isOpen={openFileModal} handleOpen={handleOpenFileModal}/>
-      <ViewPointsModal isOpen={openViewPoints} handleOpen={handleOpenViewPoints}/>
-      <div className="w-full height center flex-col gap-5">
+      <FileUploadModel
+        isOpen={openFileModal}
+        handleOpen={handleOpenFileModal}
+      />
+      <ViewPointsModal
+        isOpen={openViewPoints}
+        handleOpen={handleOpenViewPoints}
+      />
+      <div className="w-full height center flex-col gap-5 px-60">
         <div className="w-full center justify-evenly">
           <div className="ring-offset-8 ring-2 ring-[#512B81] rounded-full w-32">
->>>>>>> 4d7236572702c3b2af438b69989355b936baf317
             <img src={sprofile} className="rounded-full" />
           </div>
           <div className="center flex-col gap-3 items-start col-span-2">
@@ -44,7 +43,9 @@ export default function Student() {
               </div>
               <div className="center flex-col items-start w-[180px] border-2 border-[#512B81] rounded-xl py-1 px-2 justify-evenly text-white">
                 <span className="font-light text-[#512B81] ">Branch</span>
-                <span className="font-semibold text-black">Computer Science</span>
+                <span className="font-semibold text-black">
+                  Computer Science
+                </span>
               </div>
             </div>
             <div className="center gap-2">
@@ -52,27 +53,26 @@ export default function Student() {
               <span className="text-2xl text-[#512B81]">4</span>
             </div>
           </div>
-<<<<<<< HEAD
-          <div className="center flex-col gap-3 col-span-1">
-            <button className="center bg-[#512B81] rounded-xl w-full p-3 justify-evenly">
-=======
           <div className="center flex-col gap-3">
-            <button className="center bg-[#512B81] rounded-xl w-full p-3 justify-evenly" onClick={handleOpenFileModal}>
->>>>>>> 4d7236572702c3b2af438b69989355b936baf317
-              <img src={upload} width={30}/>
+            <button
+              className="center bg-[#512B81] rounded text-white px-4 py-2 text-sm font-medium cursor-pointer"
+              onClick={handleOpenFileModal}
+            >
+              <img src={upload} width={30} />
               <span className=" text-white">Upload Certificate</span>
             </button>
-            <button className="center bg-[#512B81] rounded-xl w-full p-3 justify-evenly" onClick={handleOpenViewPoints}>
-              <img src={graph} width={30}/>
+            <button
+              className="center bg-[#512B81] rounded text-white px-4 py-2 text-sm font-medium cursor-pointer"
+              onClick={handleOpenViewPoints}
+            >
+              <img src={graph} className="w-" />
               <span className=" text-white">View Points</span>
             </button>
           </div>
-          <div className="col-span-1"></div>
-          <div className="col-span-1"></div>
-        <div className="center shadow-[0_3px_10px_rgb(0,0,0,0.2)] text-black rounded-lg col-span-4 row-span-1">
-          <Table />
         </div>
-        <div className="col-span-1"></div>
+        <div className="w-full center shadow-[0_3px_10px_rgb(0,0,0,0.2)] text-black rounded-lg">
+        <Table />
+        </div>
       </div>
     </div>
   );
